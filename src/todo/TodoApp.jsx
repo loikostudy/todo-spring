528dfa5ad1a5bda4d1fcdc498b82c1c2bf7cf481
luissrsan <lasagna.io@outlook.com>
2024-07-08 00:43:46 -0400

    show sucess, show error, conditional rendering and react routes implemented

--- src/todo/TodoApp.jsx
+++ src/todo/TodoApp.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react"
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom"
 import "./TodoApp.css"
 export default function TodoApp(){
     return (
         <div>
         Todo Management
-<LoginComponent/>
-{/* <WelcomeComponent/> */}
+        <BrowserRouter>
+            <Routes>
+            <Route path='/' element={<LoginComponent/>}></Route>
+                <Route path='/login' element={<LoginComponent/>}></Route>
+                <Route path='/welcome' element={<WelcomeComponent/>}></Route>
+                <Route path='*' element={<ErrorComponent/>}></Route>
+            </Routes>
+        </BrowserRouter>
+
+ <WelcomeComponent/>
         </div>
     )
 }
@@ -13,8 +22,12 @@ export default function TodoApp(){
 function LoginComponent(){
    const [username,setUsername]= useState("lulu")
 const [password,setPassword] = useState("")
+const [showSuccess,setSuccess] = useState(false)
+
+const [showError,setError] = useState(false)
+const navigate = useNavigate
 const handleUsernameChange = (e)=>{
-    console.log(e.target.value)
+   
     setUsername(e.target.value)
    
 
@@ -25,8 +38,39 @@ const handlePasswordChange = (e)=>{
     setPassword(e.target.value)
    
 }
+
+const handleSubmit = ()=>{
+    if (username == 'lulu' && password == 'dummy'){
+setSuccess(true)
+setError(false)
+    }else{
+       setError(false)
+       setSuccess(true)
+       navigate('/welcome')
+    }
+}
+function ShowSuccessComponent(){
+    if(showSuccess){
+        return    <div className="success">Autheticated successfully</div>
+        
+      
+    }
+    return null
+}
+function ShowErrorComponent(){
+    if(showError){
+        return    <div className="error">Authetication failed. Please check your credentials</div>
+        
+      
+    }
+    return null
+}
     return(
         <div className='login'>
+            {showSuccess  && <div className="success">Autheticated successfully</div>}
+            {showError  && <div className="error">Authetication failed. Please check your credentials</div>}
+           <ShowSuccessComponent/>
+           <ShowErrorComponent/>
             <div className="login-form">
                 <label >User Name</label>
                 <input type="text" name="username" value={username} onChange={handleUsernameChange}/>
@@ -36,7 +80,7 @@ const handlePasswordChange = (e)=>{
                 <input type="password" name="username" value={password} onChange={ handlePasswordChange}/>
             </div>
             <div>
-<button type="button" name="login">Login</button>
+<button type="button" name="login" onClick={handleSubmit}>Login</button>
             </div>
                
                
@@ -45,10 +89,18 @@ const handlePasswordChange = (e)=>{
     )
 }
 
+function ErrorComponent(){
+    return(
+        <div className="error-component">
+           Error Component
+        </div>
+    )
+}
+
 function WelcomeComponent(){
     return(
         <div className="welcome">
-            Welcome Component
+            Welcome Lulu
         </div>
     )
 }
